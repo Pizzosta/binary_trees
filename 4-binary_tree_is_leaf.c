9ace74a050421c7b5cf29071b0dd3e8d8b532e7e
Pizzosta <pizzostor@gmail.com>
2023-04-03 09:58:15 -0700

    Anoter approach

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -13,12 +13,10 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node != NULL)
+	if (node->left == NULL || node->right == NULL)
 	{
-		binary_tree_is_leaf(node->left);
-		binary_tree_is_leaf(node->right);
-	}
 		return (1);
+	}
 	else
 		return (0);
 
