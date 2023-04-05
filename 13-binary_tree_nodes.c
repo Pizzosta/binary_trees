43779beb43d6dbb223000986355b87fb2296d7db
Pizzosta <pizzostor@gmail.com>
2023-04-05 10:19:53 -0700

    a function that counts the nodes with at least 1 child in a binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,7 +18,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (!tree->left == NULL && !tree->right == NULL)
-		return (1);
+		return (0);
 
 	left_node = binary_tree_leaves(tree->left);
 	right_node = binary_tree_leaves(tree->right);
