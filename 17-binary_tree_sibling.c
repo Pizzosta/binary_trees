e69f0056bd486fcdbc689dbf641878dd9e14e7b7
Pizzosta <pizzostor@gmail.com>
2023-04-07 17:28:31 -0700

    a function that finds the sibling of a nodegit push

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,11 +8,12 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
 	if (node == node->parent->left)
 		return (node->parent->right);
-	else
+
+	if (node == node->parent->right)
 		return (node->parent->left);
 }
