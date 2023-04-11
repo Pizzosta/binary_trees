b66fcf50ffdf04a6049b931615167cd9df7c148c
Pizzosta <pizzostor@gmail.com>
2023-04-11 06:34:03 -0700

    a function that inserts a value in a Binary Search Tree

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -14,47 +14,43 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-    if (tree == NULL)
-    {
-        return (NULL);
-    }
+	if (tree == NULL)
+	{
+		return (NULL);
+	}
 
-    /* If tree is empty, create a new node as the root node */
-    if (*tree == NULL)
-    {
-        *tree = binary_tree_node(NULL, value);
-        return (*tree);
-    }
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
 
-    /* If value is less than the current node, recursively insert to the left */
-    if (value < (*tree)->n)
-    {
-        if ((*tree)->left == NULL)
-        {
-            (*tree)->left = binary_tree_node(*tree, value);
-            return ((*tree)->left);
-        }
-        else
-        {
-            return bst_insert(&((*tree)->left), value);
-        }
-    }
-    /* If value is greater than the current node, recursively insert to the right */
-    else if (value > (*tree)->n)
-    {
-        if ((*tree)->right == NULL)
-        {
-            (*tree)->right = binary_tree_node(*tree, value);
-            return ((*tree)->right);
-        }
-        else
-        {
-            return bst_insert(&((*tree)->right), value);
-        }
-    }
-    /* If value is equal to the current node, ignore duplicate value */
-    else
-    {
-        return (*tree);
-    }
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		else
+		{
+			return (bst_insert(&((*tree)->left), value));
+		}
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
+		else
+		{
+			return (bst_insert(&((*tree)->right), value));
+		}
+	}
+	else
+	{
+		return (*tree);
+	}
 }
