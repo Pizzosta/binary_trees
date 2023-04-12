b19cc38d4e3c238d11eb59a63e9f7700bdea7c8e
Pizzosta <pizzostor@gmail.com>
2023-04-12 05:15:00 -0700

    couldnt deal with the test case error:  1. Remove a node that changes the balance 2. Remove several nodes in a row

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -2,54 +2,60 @@
 
 /**
  * bal_factor - Measures balance factor of an AVL tree
- * @tree: pointer to the tree to measure
+ * @tree: tree to go through
  *
- * Return: balance factor
+ * Return: balanced factor
  */
 int bal_factor(avl_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
-	return (binary_tree_balance((const binary_tree_t *)tree));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
 /**
  * rebalance - Rebalances an AVL tree
- * @root: pointer to the root node of the tree to rebalance
+ * @tree: pointer to the root of the AVL tree
  *
- * Return: pointer to the new root node of the tree
+ * Return: pointer to the new root of the AVL tree after rebalancing
  */
-avl_t *rebalance(avl_t *root)
+avl_t *rebalance(avl_t *tree)
 {
-	int balance, balance_left, balance_right;
+	int balance;
+	avl_t *new_root = tree;
 
-	balance = bal_factor(root);
-	balance_left = bal_factor(root->left);
-	balance_right = bal_factor(root->right);
+	balance = bal_factor(tree);
 
 	if (balance > 1)
 	{
-		if (balance_left < 0)
-			root->left = binary_tree_rotate_left(root->left);
-		root = binary_tree_rotate_right(root);
+		if (bal_factor(tree->left) >= 0)
+			new_root = binary_tree_rotate_right(tree);
+		else
+		{
+			tree->left = binary_tree_rotate_left(tree->left);
+			new_root = binary_tree_rotate_right(tree);
+		}
 	}
 	else if (balance < -1)
 	{
-		if (balance_right > 0)
-			root->right = binary_tree_rotate_right(root->right);
-		root = binary_tree_rotate_left(root);
+		if (bal_factor(tree->right) <= 0)
+			new_root = binary_tree_rotate_left(tree);
+		else
+		{
+			tree->right = binary_tree_rotate_right(tree->right);
+			new_root = binary_tree_rotate_left(tree);
+		}
 	}
 
-	return (root);
+	return (new_root);
 }
 
 /**
- * bst_remove - Removes a node from a BST tree
- * @root: pointer to the root node of the tree
- * @value: value of the node to remove
+ * bst_remove - remove a node from a BST tree
+ * @root: root of the tree
+ * @value: node with this value to remove
  *
- * Return: pointer to the new root node of the tree after removing the node
+ * Return: the tree changed
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
@@ -62,7 +68,12 @@ bst_t *bst_remove(bst_t *root, int value)
 		root->right = bst_remove(root->right, value);
 	else
 	{
-		if (root->left == NULL)
+		if (root->left == NULL && root->right == NULL)
+		{
+			free(root);
+			return (NULL);
+		}
+		else if (root->left == NULL)
 		{
 			bst_t *temp = root->right;
 
@@ -76,24 +87,27 @@ bst_t *bst_remove(bst_t *root, int value)
 			free(root);
 			return (temp);
 		}
+		else
+		{
+			bst_t *temp = root->right;
 
-		bst_t *temp = root->right;
-
-		while (temp && temp->left)
-			temp = temp->left;
-		root->n = temp->n;
-		root->right = bst_remove(root->right, temp->n);
+			while (temp && temp->left != NULL)
+				temp = temp->left;
+			root->n = temp->n;
+			root->right = bst_remove(root->right, temp->n);
+		}
 	}
 
 	return (root);
 }
 
 /**
- * avl_remove - Removes a node from an AVL tree
- * @root: pointer to the root node of the tree
- * @value: value of the node to remove
+ * avl_remove - remove a node from an AVL tree
+ * @root: pointer to root node of the tree for removing a node
+ * @value: value to remove in the tree
  *
- * Return: pointer to the new root node of the tree after removing the node
+ * Return: pointer to the new root node of the tree
+ *         after removing desired value and after rebalancing
  */
 avl_t *avl_remove(avl_t *root, int value)
 {
@@ -101,8 +115,9 @@ avl_t *avl_remove(avl_t *root, int value)
 		return (NULL);
 
 	root = (avl_t *)bst_remove((bst_t *)root, value);
-	if (root == NULL)
-		return (NULL);
 
-	return (rebalance(root));
+	/* Rebalance the AVL tree */
+	root = rebalance(root);
+
+	return (root);
 }
